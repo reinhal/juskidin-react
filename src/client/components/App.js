935420b6830d396a94e2d51e85a5acb658e26393
Lisa Reinhardt <reinhal@gmail.com>
2019-03-23 08:11:33 -0500

    first commit

---
+++ src/client/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './LandingPage';
+
+class App extends React.Component {
+  render() {
+    return (
+      <LandingPage />
+    );
+  }
+}
+
+export default App; 

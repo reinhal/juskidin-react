1b3567acc51b3193f58b8fb80e052a54fc259dca
Lisa Reinhardt <reinhal@gmail.com>
2019-03-24 07:56:07 -0500

    updated file structure

--- src/client/components/LandingPage.js
+++ src/client/components/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/LandingPage.css';
+import './styles/LandingPage.css';
 import ImageGrid from './ImageGrid';
 import Button from './Button';
 import unsplash from '../api/unsplash';

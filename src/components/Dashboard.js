e8bbede72c3c2dc865035fd78a2d21661aeee28e
Lisa Reinhardt <reinhal@gmail.com>
2019-03-25 18:37:46 -0500

    updated package.json

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        You Made It!
+      </div>
+    )
+  }
+}
+
+export default Dashboard;

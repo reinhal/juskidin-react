5d51c6d15061ba37349b93f252c6d7132f22d341
Lisa Reinhardt <reinhal@gmail.com>
2019-03-25 17:50:14 -0500

    updated test to passing

--- src/components/LandingPage.test.js
+++ src/components/LandingPage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import {LandingPage} from './LandingPage';
+import LandingPage from './LandingPage';
 
 describe('<LandingPage />', () => {
   it('Renders without crashing', () => {

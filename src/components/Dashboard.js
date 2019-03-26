816fa9a23c8b4726206ea9718dd18c0bc24b8c09
Lisa Reinhardt <reinhal@gmail.com>
2019-03-25 20:09:20 -0500

    building out static client

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import UploadForm from './UploadForm';
+import AlbumList from './AlbumList';
+import NoPhotoMessage from './NoPhotoMessage';
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div>
-        You Made It!
+        Dashboard
+        <UploadForm />
+        <NoPhotoMessage />
+        <AlbumList />
       </div>
     )
   }

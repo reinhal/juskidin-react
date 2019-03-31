5b161965f07681c4f504078d8628e0506c4f1470
Lisa Reinhardt <reinhal@gmail.com>
2019-03-31 13:18:24 -0500

    updated node modules

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Main from './Main';
 
 class App extends React.Component {
-  state = { data: null }
-
-  componentDidMount() {
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
-
   render() {
     return (
       <Router>
--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import axios from 'axios';
+import {API_BASE_URL} from '../config';
 
 export default class UploadImage extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      notes: '',
+      album: '',
+      image: ''
+    };
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    // this.onChange = this.onChange.bind(this);
+
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    let imageFormObj = new FormData();
+    
+    imageFormObj.append('title', e.target.title)
+    imageFormObj.append('notes', e.target.notes)
+    imageFormObj.append('album', e.target.album)
+    imageFormObj.append('image', e.target.files[0])
+
+    console.log(imageFormObj);
+    this.setState({
+      image: URL.createObjectURL(e.target.files[0])
+    });
+
+    axios.post(`${API_BASE_URL}/uploads`, imageFormObj)
+      .then((data) => {
+        if (data.data.success) {
+          alert('Image has been successfully uploaded');
+        }
+      })
+      .catch((err) => {
+        alert('Oops! Something went wrong')
+      })
+  }
+
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} id="imageUploadForm" className="ui form">
           <div className="field">
             <label>Title</label>
             <input type="text" name="photo-title" placeholder="Enter a title" />
@@ -15,7 +58,7 @@ export default class UploadImage extends React.Component {
           </div>
           <div className="field">
             <label>Album</label>
-            <input type="text" name="album" placeholder="Use an existing album or creat a new one" />
+            <input type="text" name="album" placeholder="Use an existing album or create a new one" />
           </div>
           <div className="field">
             <label>Image</label>

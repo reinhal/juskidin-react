2b2589a4691806f54daea16c1dcac6e82ae4bbdf
Lisa Reinhardt <reinhal@gmail.com>
2019-05-04 08:18:48 -0500

    added notes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+// https://github.com/joeeasy/cloudinaryWithMulter
+// https://medium.com/@Ugbechike/uploading-image-to-cloudinary-using-node-js-bbadd76ed0b4
+
 ReactDOM.render (
   <App />,
   document.getElementById('root')
